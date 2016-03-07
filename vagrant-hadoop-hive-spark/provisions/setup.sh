cea3ebbfb556c381d37b734313827ca85852b7ec
Tyslan <verschingellorenz@hotmail.com>
2016-03-07 10:55:30 +0100

    no clue

--- vagrant-hadoop-hive-spark/provisions/setup.sh
+++ vagrant-hadoop-hive-spark/provisions/setup.sh
@@ -110,7 +110,6 @@ sudo tar -xf spark-1.6.0-bin-hadoop2.6.tgz -C /opt
 sudo chown -R vagrant /opt/spark-1.6.0-bin-hadoop2.6
 
 echo "Configuration in spark-env.sh"
-cp -f /vagrant/resources/spark-env.sh /opt/spark-1.6.0-bin-hadoop2.6/conf
-cp -f /vagrant/resources/spark-defaults.conf /opt/spark-1.6.0-bin-hadoop2.6/conf
+cp /opt/spark-1.6.0-bin-hadoop2.6/conf/spark-env.sh.template /opt/spark-1.6.0-bin-hadoop2.6/conf/spark-env.sh
 
 echo "I'm alive and kicking"
