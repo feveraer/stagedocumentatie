871e40384f41233fc66ca4dc01c50e7300d625ef
Tyslan <verschingellorenz@hotmail.com>
2016-03-08 10:05:44 +0100

    sql files for qbus import - Part 3

---
+++ vagrant-hadoop-hive-spark/resources/sql_for_qbus_import/create_EXT_table_Controllers.sql
@@ -0,0 +1,18 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS Controllers(
+    Id BIGINT,
+    SerialNumber STRING,
+    ApiKey STRING,
+    VerificationKey STRING,
+    Name STRING,
+    MacAdress STRING,
+    FirmwareVersion STRING,
+    RegisteredAt TIMESTAMP,
+    LastAcces TIMESTAMP,
+    Activated BOOLEAN,
+    IPAdress STRING,
+    ReceivingRate DECIMAL,
+    Timezone STRING)
+  ROW FORMAT DELIMITED
+  FIELDS TERMINATED BY ','
+  STORED AS TEXTFILE
+  location '/folder/outputlogs';
