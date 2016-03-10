fbfc5089f071c7370fdd066cdb326c385f97cb68
feveraer <frederic.everaert@gmail.com>
2016-03-10 15:09:38 +0100

    added qbus queries

--- QBus sql queries.sql
+++ QBus sql queries.sql
@@ -31,3 +31,26 @@ order by o.Uid, l.Time
 
 select COUNT(*)
 from OutputLogs
+
+select top 10000 ol.time settime, oghd.time measuredtime, 
+ol.value setvalue, oghd.value measuredvalue, l.userid, o.id, l.name 
+from A_QbusCloud.dbo.OutputGraphHourData oghd
+join A_QbusCloud.dbo.Outputs o on oghd.outputid = o.id
+join A_QbusCloud.dbo.Location l on o.locationid = l.id
+left join A_QbusCloud.dbo.OutputLogs ol on o.id = ol.outputid
+join A_QbusCloud.dbo.Types t on o.typeid = t.id
+
+select ol.time time, ol.value value, l.userid, o.id, l.name
+from OutputLogs ol
+join Outputs o on ol.OutputID = o.id
+join Location l on o.locationid = l.id
+join Types t on o.typeid = t.id
+where l.Userid = '53' and t.Name = 'THERMO'
+union
+select oghd.time, oghd.value, l.userid, o.id, l.name 
+from OutputGraphHourData oghd
+join Outputs o on oghd.outputid = o.id
+join Location l on o.locationid = l.id
+join Types t on o.typeid = t.id
+where l.Userid = '53'
+order by time desc, name
