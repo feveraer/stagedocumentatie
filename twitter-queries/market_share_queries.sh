2896072c94ea23313a14071d73d100152fc4dd0c
feveraer <frederic.everaert@gmail.com>
2016-03-15 17:03:14 +0100

    added market share queries file

---
+++ twitter-queries/market_share_queries.sh
@@ -0,0 +1,40 @@
+# bash/bin
+
+DATABASE_NAME=""
+
+while getopts ":d:" opt; do
+  case $opt in
+    d)
+	  DATABASE_NAME = "${OPTARG}"
+	  ;;
+	\?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+MYSQL_USER="root"
+MYSQL_PASSWORD="Ugent2016"
+TARGET_FOLDER=/home/twitter/csv/
+
+for tb in $(mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -sN -e "SHOW TABLES")
+do
+  echo "Alter table ${tb}"
+  mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "alter table ${tb} modify Tweet varchar(300)"
+  mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "alter table ${tb} CHANGE stockticker stocksymbol varchar(10)"
+  mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "alter table ${tb} modify stocksymbol varchar(10)"
+
+  echo "Replace new lines in table ${tb}"
+  mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "update ${tb} set Tweet = replace(Tweet, '\n','\\\\n')"
+  mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "update ${tb} set UserName = replace(UserName, '\n','\\\\n')"
+   mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "update ${tb} set stocksymbol = replace(stocksymbol, '\n','\\\\n')"
+
+  echo "Export table ${tb}"
+  mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "select * from ${tb} into outfile '${TARGET_FOLDER}${tb}.csv' fields enclosed by '' terminated by '´~¨\`^' escaped by '' lines terminated by '\r\n';"
+done
