b622f92c26f5d3ddd3883f71a8883dbb4b16fabf
Tyslan <verschingellorenz@hotmail.com>
2016-03-16 13:22:38 +0100

    script edited for testing

--- twitter-queries/market_share_queries.sh
+++ twitter-queries/market_share_queries.sh
@@ -25,13 +25,8 @@ MYSQL_USER="root"
 MYSQL_PASSWORD="Ugent2016"
 
 declare -a keys=(
-  # Consoles
-  'teamplaystation' 'goplaystation' 'teamsony' 'gosony' 'playstation' 'ps' 'ps4' 'sonyplaystation'
-  'teammicrosoft' 'teamxbox' 'goxbox' 'gomicrosoft' 'xbox' 'xone' 'xbone'
-  'teamnintendo' 'gonintendo' 'wii' 'wiiu'
-  # Fast food restaurants
-  'mcdonalds' 'mcdo' 'happymeal' 'imlovinit' 'bigmac' 'mcdstories' 'dollarmenu' 'mccafe' 'teammcdo'
-  'kfc' 'kentuckyfriedchicken' 'howdoyoukfc' 'doublicious' 'chickenlittles' 'famousbowl' 'teamkfc,yumbrands'
+  'ps'
+  'mcdo'
   );
 
 for tb in $(mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -sN -e "SHOW TABLES")
@@ -42,7 +37,7 @@ do
     echo "select min(timestamp),max(timestamp),count(*) from ${tb} where upper(tweet) like '%${i}%' and timestamp > '2015-01-01 00:00:00' and timestamp < '2015-07-01 00:00:00' into outfile '/home/twitter/query_results/${i}_count.txt'" >> /home/twitter/query_results/${i}.txt
     sudo mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "select min(timestamp),max(timestamp),count(*) from ${tb} where upper(tweet) like '%${i}%' and timestamp > '2015-01-01 00:00:00' and timestamp < '2015-07-01 00:00:00' into outfile '/home/twitter/query_results/${i}_count.txt'"
     cat /home/twitter/query_results/${i}_count.txt >> /home/twitter/query_results/${i}.txt
-    echo "\n" >> /home/twitter/query_results/${i}.txt
+    echo -e >> /home/twitter/query_results/${i}.txt
     rm -f /home/twitter/query_results/${i}_count.txt
     cat cat /home/twitter/query_results/${i}_count.txt >> /home/twitter/query_results/${i}.txt >> cat /home/twitter/query_results/${i}_count.txt >> /home/twitter/query_results/result.txt
     rm -f cat /home/twitter/query_results/${i}_count.txt >> /home/twitter/query_results/${i}.txt
