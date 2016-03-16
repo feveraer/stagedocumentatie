7884032d7c869e3d44188148ee32f57ba80b6f34
Tyslan <verschingellorenz@hotmail.com>
2016-03-16 11:43:41 +0100

    Add search query

---
+++ twitter-queries/mysqlTwitter-Q1-test.sh
@@ -0,0 +1,26 @@
+#bin/bash
+
+tb="collecttweetuni"
+MYSQL_USER="root"
+MYSQL_PASSWORD="Ugent2016"
+DATABASE_NAME="ugenttwitter5"
+
+declare -a keys=(
+  # Consoles
+  'teamplaystation' 'goplaystation' 'teamsony' 'gosony' 'playstation' 'ps' 'ps4' 'sonyplaystation'
+  'teammicrosoft' 'teamxbox' 'goxbox' 'gomicrosoft' 'xbox' 'xone' 'xbone'
+  'teamnintendo' 'gonintendo' 'wii' 'wiiu'
+  # Fast food restaurants
+  'mcdonalds' 'mcdo' 'happymeal' 'imlovinit' 'bigmac' 'mcdstories' 'dollarmenu' 'mccafe' 'teammcdo'
+  'kfc' 'kentuckyfriedchicken' 'howdoyoukfc' 'doublicious' 'chickenlittles' 'famousbowl' 'teamkfc,yumbrands'
+  );
+
+for i in "${keys[@]}"
+do
+  echo "${i}" >> /home/twitter/query_results/${i}.txt
+  echo "select min(timestamp),max(timestamp),count(*) from ${tb} where upper(tweet) like '%${i}%' and timestamp > '2015-01-01 00:00:00' and timestamp < '2015-07-01 00:00:00' into outfile '/home/twitter/query_results/${i}_count.txt'" >> /home/twitter/query_results/${i}.txt
+  sudo mysql -B -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -e "select min(timestamp),max(timestamp),count(*) from ${tb} where upper(tweet) like '%${i}%' and timestamp > '2015-01-01 00:00:00' and timestamp < '2015-07-01 00:00:00' into outfile '/home/twitter/query_results/${i}_count.txt'"
+  cat /home/twitter/query_results/${i}_count.txt >> /home/twitter/query_results/${i}.txt
+  echo "\n" >> /home/twitter/query_results/${i}.txt
+  rm -f /home/twitter/query_results/${i}_count.txt
+done
