0d883223f3c563f025fc2883d5ecc126b484eef0
Tyslan <verschingellorenz@hotmail.com>
2016-03-21 11:17:32 +0100

    script for import is more dynamic

--- twitter-export/twitter_export.sh
+++ twitter-export/twitter_export.sh
@@ -1,8 +1,39 @@
-#!/bash/bin
+#!/bin/bash
 
-DATABASE_NAME="collecttweet"
-MYSQL_USER="root"
-MYSQL_PASSWORD="Ugent2016"
+DATABASE_NAME=""
+
+usage() {
+  echo "This script needs to know the database name."
+  echo -e "\nUsage: $0 -d <database_name> \n"
+}
+
+if [ $# -le 1  ]
+then
+  usage
+  exit 1
+fi
+
+while getopts ":d:" opt; do
+  case $opt in
+    d)
+      DATABASE_NAME="${OPTARG}"
+      ;;
+    \?)
+      echo "invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "option -$OPTARG requires an argument" >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo
+read -r -p "MySQL username: " MYSQL_USER
+read -s -r -p "MySQL password: " MYSQL_PASSWORD
+echo
+
 TARGET_FOLDER=/home/twitter/csv/
 
 for tb in $(mysql -u $MYSQL_USER --password=$MYSQL_PASSWORD $DATABASE_NAME -sN -e "SHOW TABLES")
