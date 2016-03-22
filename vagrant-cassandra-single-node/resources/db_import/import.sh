7d0e1f673f17fd97883b9a4d40615d6d38951b5e
feveraer <frederic.everaert@gmail.com>
2016-03-22 10:27:30 +0100

    cassandra import csv stuff

--- vagrant-cassandra-single-node/resources/db_import/import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cqlsh -f import_Controllers.cql
-cqlsh -f import_Locations.cql
-cqlsh -f import_OutputGraphHourData.cql
-cqlsh -f import_Outputlogs.cql
-cqlsh -f import_Outputs.cql
-cqlsh -f import_Types.cql
