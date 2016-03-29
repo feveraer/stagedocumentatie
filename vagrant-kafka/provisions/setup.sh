e32f11e28ef85beff0cd18b34229e9ba5ea95b7a
feveraer <frederic.everaert@gmail.com>
2016-03-29 13:46:17 +0200

    updated provision file

--- vagrant-kafka/provisions/setup.sh
+++ vagrant-kafka/provisions/setup.sh
@@ -5,9 +5,18 @@ echo "| Updating the source list ...      |"
 echo " ==================================="
 sudo apt-get -qq update
 
-# echo
-# echo " ==================================="
-# echo "| Installing Java 7 ...             |"
-# echo " ==================================="
-# sudo apt-get install -y default-jdk > /dev/null
+echo " ==================================="
+echo "| Installing Java 7 ...             |"
+echo " ==================================="
+sudo apt-get install -y default-jdk > /dev/null
+
+echo " ==================================="
+echo "| Downloading Kafka ...             |"
+echo " ==================================="
+wget --progress=bar:force http://apache.belnet.be/kafka/0.9.0.0/kafka_2.11-0.9.0.0.tgz
+
+echo " ==================================="
+echo "| Extracting Kafka ...              |"
+echo " ==================================="
+tar -xzf kafka_2.11-0.9.0.0.tgz
 
