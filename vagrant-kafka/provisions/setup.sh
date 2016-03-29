8939b0b6d304ae1670fd01e5baeb8783a6175986
feveraer <frederic.everaert@gmail.com>
2016-03-29 12:22:34 +0200

    init kafka machine provision script

---
+++ vagrant-kafka/provisions/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo " ==================================="
+echo "| Updating the source list ...      |"
+echo " ==================================="
+sudo apt-get -qq update
+
+echo
+echo " ==================================="
+echo "| Installing Java 7 ...             |"
+echo " ==================================="
+sudo apt-get install -y default-jdk > /dev/null
+
+
