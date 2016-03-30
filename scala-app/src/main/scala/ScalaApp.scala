d19a7ff17eec3091c68e2cc7dc92f86c60fc4920
feveraer <frederic.everaert@gmail.com>
2016-03-30 11:38:19 +0200

    scala-app: spark test [working]

--- scala-app/src/main/scala/ScalaApp.scala
+++ scala-app/src/main/scala/ScalaApp.scala
@@ -8,7 +8,11 @@ import org.apache.spark.SparkConf
 
 object ScalaApp {
   def main(args: Array[String]) {
-    val logFile = "resources/test.txt" // Should be some file on your system
+    // BUG FIX WINDOWS: spark needs to be able to locate winutils.exe
+    // http://qnalist.com/questions/4994960/run-spark-unit-test-on-windows-7
+    System.setProperty("hadoop.home.dir", "d:\\winutil\\")
+
+    val logFile = "src/main/resources/test.txt" // Should be some file on your system
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
