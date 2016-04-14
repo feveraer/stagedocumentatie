398f8518e51c4b0d16d3647ec13d9cbd19022e24
Tyslan <verschingellorenz@hotmail.com>
2016-04-14 14:08:17 +0200

    Time class [WIP]

--- neural_network/src/main/scala-2.11/testsetann/Time.scala
+++ neural_network/src/main/scala-2.11/testsetann/Time.scala
@@ -1,30 +1,62 @@
 package testsetann
 
-import java.util.{Calendar, Date, GregorianCalendar}
+import java.util.{Calendar, GregorianCalendar}
 
 /**
   * Created by Lorenz on 13/04/2016.
   */
+object Time {
+  val MILLIS_IN_SECOND: Long = 1000
+  val MILLIS_IN_MINUTE: Long = MILLIS_IN_SECOND * 60
+  val MILLIS_IN_HOUR: Long = MILLIS_IN_MINUTE * 60
+  val MILLIS_IN_DAY: Long = MILLIS_IN_HOUR * 24
+  val MILLIS_IN_31_DAYS_MONTH: Long = MILLIS_IN_DAY * 31
+  val MILLIS_IN_30_DAYS_MONTH: Long = MILLIS_IN_DAY * 30
+  val MILLIS_IN_29_DAYS_MONTH: Long = MILLIS_IN_DAY * 29
+  val MILLIS_IN_28_DAYS_MONTH: Long = MILLIS_IN_DAY * 28
+  val MILLIS_IN_NORMAL_YEAR: Long = 7 * MILLIS_IN_31_DAYS_MONTH + 4 * MILLIS_IN_30_DAYS_MONTH + MILLIS_IN_28_DAYS_MONTH
+  val MILLIS_IN_LEAP_YEAR: Long = 7 * MILLIS_IN_31_DAYS_MONTH + 4 * MILLIS_IN_30_DAYS_MONTH + MILLIS_IN_29_DAYS_MONTH
 
-case class Time (year: Int, month: Int, day: Int, hour: Int, minutes: Int, seconds: Int) {
+  private val DAYS_IN_MONTH_NORMAL_YEAR = (31,28,31,30,31,30,31,31,30,31,30,31)
+  private val DAYS_IN_MONTH_LEAP_YEAR = (31,29,31,30,31,30,31,31,30,31,30,31)
 
-  def difference(time:Time): Time = {
+  def DAYS_IN_MONTH(year: Int, month: Int): Unit ={
+    
+  }
+
+  def IS_LEAP_YEAR(year: Int): Boolean = {
+    val cal = Calendar.getInstance()
+    cal.set(Calendar.YEAR, year)
+    cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365
+  }
+}
+
+case class Time(year: Int, month: Int, day: Int, hour: Int, minutes: Int, seconds: Int) {
+
+  def difference(time: Time): Time = {
     val d1 = new GregorianCalendar(year + 1900, month, day, hour, minutes, seconds)
     val d2 = new GregorianCalendar(time.year + 1900, time.month, time.day,
       time.hour, time.minutes, time.seconds)
 
     val diffInMillis = d2.getTimeInMillis - d1.getTimeInMillis
-    val diffAsDate = new GregorianCalendar()
-    diffAsDate.setTimeInMillis(diffInMillis)
 
-    val diff = Time(
-      diffAsDate.get(Calendar.YEAR),
-      diffAsDate.get(Calendar.MONTH),
-      diffAsDate.get(Calendar.DAY_OF_MONTH),
-      diffAsDate.get(Calendar.HOUR),
-      diffAsDate.get(Calendar.MINUTE),
-      diffAsDate.get(Calendar.SECOND)
+    var yearDiff = 0
+    var monthDiff = 0
+    var dayDiff = (diffInMillis)
+    val hourDiff = (diffInMillis / Time.MILLIS_IN_HOUR % 24).toInt
+    val minuteDiff = (diffInMillis / Time.MILLIS_IN_MINUTE % 60).toInt
+    val secondDiff = (diffInMillis / Time.MILLIS_IN_SECOND % 60).toInt
+
+
+    val diff = new Time(
+      yearDiff,
+      monthDiff,
+      dayDiff,
+      hourDiff,
+      minuteDiff,
+      secondDiff
     )
+
     diff
   }
 }
--- neural_network/src/test/scala-2.11/TimeTest.scala
+++ neural_network/src/test/scala-2.11/TimeTest.scala
@@ -8,38 +8,122 @@ import testsetann.Time
   */
 
 class TimeTest extends AssertionsForJUnit {
-  @Test def testDifference(): Unit ={
-    val time1 = new Time(23,10)
-    val time2 = new Time(0,1)
-    val time3 = new Time(23,20)
-    val time4 = new Time(22,30)
-    val time5 = new Time(23,40)
-    val time6 = new Time(22,50)
-    val time7 = new Time(22,40)
-    val time8 = new Time(23,9)
-    val time9 = new Time(22,9)
+  @Test def testSecondsDifference(): Unit = {
+    val time1 = new Time(2015, 12, 31, 23, 10, 0)
+    val time2 = new Time(2015, 12, 31, 23, 10, 10)
+    val time3 = new Time(2015, 12, 31, 23, 9, 50)
+
+    val time4 = new Time(2015, 12, 31, 23, 10, 1)
+    val time5 = new Time(2015, 12, 31, 23, 10, 11)
+    val time6 = new Time(2015, 12, 31, 23, 9, 51)
+
+    val solution1 = new Time(0, 0, 0, 0, 0, 10)
 
     val diff1 = time1.difference(time2)
-    val diff2 = time1.difference(time3)
-    val diff3 = time4.difference(time5)
-    val diff4 = time6.difference(time1)
-    val diff5 = time7.difference(time1)
-    val diff6 = time1.difference(time8)
-    val diff7 = time1.difference(time9)
+    val diff2 = time3.difference(time1)
 
-    assertEquals(0, diff1.hour)
-    assertEquals(51, diff1.minutes)
-    assertEquals(0, diff2.hour)
-    assertEquals(10, diff2.minutes)
-    assertEquals(1, diff3.hour)
-    assertEquals(10, diff3.minutes)
-    assertEquals(0, diff4.hour)
-    assertEquals(20, diff4.minutes)
-    assertEquals(0, diff5.hour)
-    assertEquals(30, diff5.minutes)
-    assertEquals(23, diff6.hour)
-    assertEquals(59, diff6.minutes)
-    assertEquals(22, diff7.hour)
-    assertEquals(59, diff7.minutes)
+    val diff3 = time4.difference(time5)
+    val diff4 = time6.difference(time4)
+
+
+    assertEquals(solution1, diff1)
+    assertEquals(solution1, diff2)
+
+    assertEquals(solution1, diff3)
+    assertEquals(solution1, diff4)
   }
+
+  @Test def testMinutesDifference(): Unit = {
+    val time1 = new Time(2015, 12, 31, 23, 0, 0)
+    val time2 = new Time(2015, 12, 31, 23, 10, 0)
+    val time3 = new Time(2015, 12, 31, 22, 50, 0)
+
+    val time4 = new Time(2015, 12, 31, 23, 1, 0)
+    val time5 = new Time(2015, 12, 31, 23, 11, 0)
+    val time6 = new Time(2015, 12, 31, 22, 51, 0)
+
+    val solution1 = new Time(0, 0, 0, 0, 10, 0)
+
+    val diff1 = time1.difference(time2)
+    val diff2 = time3.difference(time1)
+
+    val diff3 = time4.difference(time5)
+    val diff4 = time6.difference(time4)
+
+
+    assertEquals(solution1, diff1)
+    assertEquals(solution1, diff2)
+
+    assertEquals(solution1, diff3)
+    assertEquals(solution1, diff4)
+  }
+
+  @Test def testHourDifference(): Unit = {
+    val time1 = new Time(2015, 12, 15, 17, 0, 0)
+    val time2 = new Time(2015, 12, 15, 19, 0, 0)
+    val time3 = new Time(2015, 12, 15, 15, 0, 0)
+
+    val time4 = new Time(2015, 12, 15, 23, 0, 0)
+    val time5 = new Time(2015, 12, 16, 1, 0, 0)
+    val time6 = new Time(2015, 12, 15, 21, 0, 0)
+
+    val solution1 = new Time(0, 0, 0, 2, 0, 0)
+
+    val diff1 = time1.difference(time2)
+    val diff2 = time3.difference(time1)
+
+    val diff3 = time4.difference(time5)
+    val diff4 = time6.difference(time4)
+
+
+    assertEquals(solution1, diff1)
+    assertEquals(solution1, diff2)
+
+    assertEquals(solution1, diff3)
+    assertEquals(solution1, diff4)
+  }
+
+  @Test def testDayDifference(): Unit = {
+    val time1 = new Time(2015, 12, 15, 0, 0, 0)
+    val time2 = new Time(2015, 12, 17, 0, 0, 0)
+    val time3 = new Time(2015, 12, 13, 0, 0, 0)
+
+    val time4 = new Time(2015, 10, 30, 0, 0, 0)
+    val time5 = new Time(2015, 11, 2, 0, 0, 0)
+
+    val time6 = new Time(2015, 9, 30, 0, 0, 0)
+    val time7 = new Time(2015, 10, 2, 0, 0, 0)
+
+    val time8 = new Time(2015, 2, 28, 0, 0, 0)
+    val time9 = new Time(2015, 3, 2, 0, 0, 0)
+
+    val time10 = new Time(2016, 2, 28, 0, 0, 0)
+    val time11 = new Time(2015, 3, 1, 0, 0, 0)
+
+    val solution1 = new Time(0, 0, 2, 0, 0, 0)
+
+    val diff1 = time1.difference(time2)
+    val diff2 = time3.difference(time1)
+
+    val diff3 = time4.difference(time5)
+
+    val diff4 = time6.difference(time7)
+
+    val diff5 = time8.difference(time9)
+
+    val diff6 = time10.difference(time11)
+
+
+//    assertEquals(solution1, diff1)
+//    assertEquals(solution1, diff2)
+//
+//    assertEquals(solution1, diff3)
+
+//    assertEquals(solution1, diff4)
+
+//    assertEquals(solution1, diff5)
+
+//    assertEquals(solution1, diff6)
+  }
+
 }
