4b4d0c37010d4db66ed3a5d020994426dc9418ce
feveraer <frederic.everaert@gmail.com>
2016-04-14 10:47:51 +0200

    nn: time class refactor

--- neural_network/src/main/scala-2.11/testsetann/Time.scala
+++ neural_network/src/main/scala-2.11/testsetann/Time.scala
@@ -9,36 +9,22 @@ import java.util.{Calendar, Date, GregorianCalendar}
 case class Time (year: Int, month: Int, day: Int, hour: Int, minutes: Int, seconds: Int) {
 
   def difference(time:Time): Time = {
-//    var hourDiff=0
-//    var minuteDiff=0
-//
-//    if(time.hour >= hour){
-//      hourDiff = time.hour - hour
-//    } else {
-//      hourDiff = 24 - hour + time.hour
-//    }
-//    if(time.minutes >= minutes) {
-//      minuteDiff = time.minutes - minutes
-//    } else {
-//      hourDiff -= 1
-//      minuteDiff = 60 - (minutes - time.minutes)
-//    }
-//    if(hourDiff == -1) {
-//      hourDiff = 23
-//    }
-//
-//    new Time(hourDiff, minuteDiff)
-
     val d1 = new GregorianCalendar(year + 1900, month, day, hour, minutes, seconds)
     val d2 = new GregorianCalendar(time.year + 1900, time.month, time.day,
       time.hour, time.minutes, time.seconds)
 
     val diffInMillis = d2.getTimeInMillis - d1.getTimeInMillis
-    // year, month, day, hour, minutes, seconds
-    val diff = (
+    val diffAsDate = new GregorianCalendar()
+    diffAsDate.setTimeInMillis(diffInMillis)
 
+    val diff = Time(
+      diffAsDate.get(Calendar.YEAR),
+      diffAsDate.get(Calendar.MONTH),
+      diffAsDate.get(Calendar.DAY_OF_MONTH),
+      diffAsDate.get(Calendar.HOUR),
+      diffAsDate.get(Calendar.MINUTE),
+      diffAsDate.get(Calendar.SECOND)
     )
-
-    null
+    diff
   }
 }
