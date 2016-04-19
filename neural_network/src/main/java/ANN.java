1444cd7931de58cf8a8e072b7d2d748afee94143
feveraer <frederic.everaert@gmail.com>
2016-04-19 15:14:47 +0200

    nn: updated ANN.java

--- neural_network/src/main/java/ANN.java
+++ neural_network/src/main/java/ANN.java
@@ -5,13 +5,21 @@ import ann.NeuralNetwork;
  * Created by Lorenz on 13/04/2016.
  */
 public class ANN {
-    public static void main(String[] args) {
-        NetworkTrainer networkTrainer = new NetworkTrainer("src/main/resources/TrainingsSet.tsv");
-        networkTrainer.train();
-        networkTrainer.exportModel();
 
-        NeuralNetwork ann = new NeuralNetwork();
+    private NetworkTrainer networkTrainer = new NetworkTrainer("src/main/resources/TrainingsSet.tsv");
+    private NeuralNetwork ann;
+
+    public static void main(String[] args) {
+        new ANN().run();
+    }
+
+    public void run() {
+        ann = new NeuralNetwork();
         ann.loadModel("src/main/resources/network/encogNormalizationHelper.eg", "src/main/resources/network/encogBestMethod.eg");
         ann.predictFromCSV("src/main/resources/TrainingsSet.tsv");
+    }
+
+    public void draw() {
+
     }
 }
