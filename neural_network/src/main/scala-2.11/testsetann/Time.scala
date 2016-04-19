7a863b4279529730a803ace887578cc3f9bdf1ac
Tyslan <verschingellorenz@hotmail.com>
2016-04-19 13:19:14 +0200

    Time class [DONE]

--- neural_network/src/main/scala-2.11/testsetann/Time.scala
+++ neural_network/src/main/scala-2.11/testsetann/Time.scala
@@ -1,77 +1,84 @@
 package testsetann
 
 import java.time.temporal.ChronoUnit
-import java.time.{LocalDate, LocalTime, Period}
-import java.util.{Calendar, GregorianCalendar}
+import java.time.{LocalDate, LocalTime}
 
 /**
   * Created by Lorenz on 13/04/2016.
   */
 object Time {
-  val MILLIS_IN_SECOND: Long = 1000
-  val MILLIS_IN_MINUTE: Long = MILLIS_IN_SECOND * 60
-  val MILLIS_IN_HOUR: Long = MILLIS_IN_MINUTE * 60
-  val MILLIS_IN_DAY: Long = MILLIS_IN_HOUR * 24
-  val MILLIS_IN_31_DAYS_MONTH: Long = MILLIS_IN_DAY * 31
-  val MILLIS_IN_30_DAYS_MONTH: Long = MILLIS_IN_DAY * 30
-  val MILLIS_IN_29_DAYS_MONTH: Long = MILLIS_IN_DAY * 29
-  val MILLIS_IN_28_DAYS_MONTH: Long = MILLIS_IN_DAY * 28
-  val MILLIS_IN_NORMAL_YEAR: Long = 7 * MILLIS_IN_31_DAYS_MONTH + 4 * MILLIS_IN_30_DAYS_MONTH + MILLIS_IN_28_DAYS_MONTH
-  val MILLIS_IN_LEAP_YEAR: Long = 7 * MILLIS_IN_31_DAYS_MONTH + 4 * MILLIS_IN_30_DAYS_MONTH + MILLIS_IN_29_DAYS_MONTH
-
   private val DAYS_IN_MONTH_NORMAL_YEAR = Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
   private val DAYS_IN_MONTH_LEAP_YEAR = Array(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 
   def DAYS_IN_MONTH(year: Int, month: Int): Int = {
     if (IS_LEAP_YEAR(year)) {
-      DAYS_IN_MONTH_LEAP_YEAR(month - 1)
+      return DAYS_IN_MONTH_LEAP_YEAR(month - 1)
     }
     DAYS_IN_MONTH_NORMAL_YEAR(month - 1)
   }
 
   def IS_LEAP_YEAR(year: Int): Boolean = {
-    val cal = Calendar.getInstance()
-    cal.set(Calendar.YEAR, year)
-    cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365
+    val date = LocalDate.of(year, 2, 28)
+    date.isLeapYear
   }
 }
 
 case class Time(year: Int, month: Int, day: Int, hour: Int, minutes: Int, seconds: Int) {
 
   def difference(time: Time): Time = {
-    //    val d1 = new GregorianCalendar(year + 1900, month, day, hour, minutes, seconds)
-    //    val d2 = new GregorianCalendar(time.year + 1900, time.month, time.day,
-    //      time.hour, time.minutes, time.seconds)
-
+    // Bouw datum en tijd
     val date1 = LocalDate.of(year, month, day)
     val time1 = LocalTime.of(hour, minutes, seconds)
 
     val date2 = LocalDate.of(time.year, time.month, time.day)
     val time2 = LocalTime.of(time.hour, time.minutes, time.seconds)
 
-    //    val diffInMillis = d2.getTimeInMillis - d1.getTimeInMillis
-    //
-    //    var yearDiff = 0
-    //    var monthDiff = 0
-    //    val dayDiff = (diffInMillis / Time.MILLIS_IN_DAY % Time.DAYS_IN_MONTH(year, month)).toInt
-    //    val hourDiff = (diffInMillis / Time.MILLIS_IN_HOUR % 24).toInt
-    //    val minuteDiff = (diffInMillis / Time.MILLIS_IN_MINUTE % 60).toInt
-    //    val secondDiff = (diffInMillis / Time.MILLIS_IN_SECOND % 60).toInt
-
-
+    // verschillen berekenen
     val yearDiff = date1.until(date2, ChronoUnit.YEARS).toInt
-    val monthDiff = date1.until(date2, ChronoUnit.MONTHS).toInt
+    val monthDiff = (date1.until(date2, ChronoUnit.MONTHS) % 12).toInt
     var dayDiff = date1.until(date2, ChronoUnit.DAYS).toInt
     var hourDiff = (time1.until(time2, ChronoUnit.HOURS) % 24).toInt
-    val minuteDiff = (time1.until(time2, ChronoUnit.MINUTES) % 60).toInt
-    val secondDiff = (time1.until(time2, ChronoUnit.SECONDS) % 60).toInt
+    var minuteDiff = (time1.until(time2, ChronoUnit.MINUTES) % 60).toInt
+    var secondDiff = (time1.until(time2, ChronoUnit.SECONDS) % 60).toInt
+
+    if (secondDiff < 0) {
+      secondDiff = 60 + secondDiff
+      minuteDiff = minuteDiff - 1
+    }
+
+    if (minuteDiff < 0) {
+      minuteDiff = 60 + minuteDiff
+      hourDiff = hourDiff - 1
+    }
 
     if (hourDiff < 0) {
       hourDiff = 24 + hourDiff
     }
 
+    if (dayDiff < 0) {
+      dayDiff = Time.DAYS_IN_MONTH(year, month) + dayDiff
+    }
+
     if (time2.isBefore(time1)) {
       dayDiff = dayDiff - 1
+    }
+
+    if (monthDiff > 0 || yearDiff > 0) {
+      var dateIter = LocalDate.of(date1.getYear, date1.getMonth, date2.getDayOfMonth)
+      var sum = 0
+
+      while (dateIter.isBefore(date2)) {
+        val year = dateIter.getYear
+        val month = dateIter.getMonthValue
+        sum = sum + Time.DAYS_IN_MONTH(year, month)
+        dateIter = dateIter.plus(1, ChronoUnit.MONTHS)
+      }
+
+      dayDiff = dayDiff - sum
+
+      if(dayDiff < 0){
+        dayDiff = Time.DAYS_IN_MONTH(date1.getYear, date1.getMonthValue) + dayDiff + 1
+      }
     }
 
     val diff = new Time(
--- neural_network/src/test/scala-2.11/TimeTest.scala
+++ neural_network/src/test/scala-2.11/TimeTest.scala
@@ -126,47 +126,77 @@ class TimeTest extends AssertionsForJUnit {
     assertEquals(solution1, diff6)
   }
 
-  @Test def testDayDifference(): Unit = {
-    val time1 = new Time(2015, 12, 15, 0, 0, 0)
-    val time2 = new Time(2015, 12, 17, 0, 0, 0)
-    val time3 = new Time(2015, 12, 13, 0, 0, 0)
+  @Test def testMonthDifference(): Unit = {
+    val time1 = new Time(2015, 10, 1, 0, 0, 0)
+    val time2 = new Time(2015, 12, 1, 0, 0, 0)
+    val time3 = new Time(2015, 8, 1, 0, 0, 0)
 
-    val time4 = new Time(2015, 10, 30, 0, 0, 0)
-    val time5 = new Time(2015, 11, 1, 0, 0, 0)
+    val time4 = new Time(2015, 12, 1, 0, 0, 0)
+    val time5 = new Time(2016, 2, 1, 0, 0, 0)
 
-    val time6 = new Time(2015, 9, 30, 0, 0, 0)
-    val time7 = new Time(2015, 10, 2, 0, 0, 0)
-
-    val time8 = new Time(2015, 2, 28, 0, 0, 0)
-    val time9 = new Time(2015, 3, 2, 0, 0, 0)
-
-    val time10 = new Time(2016, 2, 28, 0, 0, 0)
-    val time11 = new Time(2016, 3, 1, 0, 0, 0)
-
-    val solution1 = new Time(0, 0, 2, 0, 0, 0)
+    val solution1 = new Time(0, 2, 0, 0, 0, 0)
 
     val diff1 = time1.difference(time2)
     val diff2 = time3.difference(time1)
 
     val diff3 = time4.difference(time5)
 
-    val diff4 = time6.difference(time7)
-
-    val diff5 = time8.difference(time9)
-
-    val diff6 = time10.difference(time11)
-
-
     assertEquals(solution1, diff1)
     assertEquals(solution1, diff2)
 
     assertEquals(solution1, diff3)
+  }
 
-    assertEquals(solution1, diff4)
+  @Test def testYearDifference(): Unit = {
+    val time1 = new Time(2015, 10, 1, 0, 0, 0)
+    val time2 = new Time(2016, 10, 1, 0, 0, 0)
+    val time3 = new Time(2014, 10, 1, 0, 0, 0)
 
-    assertEquals(solution1, diff5)
+    val solution1 = new Time(1, 0, 0, 0, 0, 0)
 
-    assertEquals(solution1, diff6)
+    val diff1 = time1.difference(time2)
+    val diff2 = time3.difference(time1)
+
+    assertEquals(solution1, diff1)
+    assertEquals(solution1, diff2)
+  }
+
+  @Test def testRandomDifference(): Unit = {
+    val time1 = new Time(2017, 10, 2, 10, 12, 50)
+    val time2 = new Time(2016, 10, 1, 0, 0, 0)
+
+    val solution1 = new Time(1, 0, 1, 10, 12, 50)
+
+    val diff1 = time2.difference(time1)
+
+    assertEquals(solution1, diff1)
+
+    val time3 = new Time(50, 10, 2, 10, 12, 50)
+    val time4 = new Time(51, 10, 1, 0, 0, 0)
+    val solution2 = new Time(0, 11, 30, 13, 47, 10)
+
+    val diff2 = time3.difference(time4)
+
+    assertEquals(solution2, diff2)
+
+    val time5 = new Time(2011, 9, 21, 8, 33, 0)
+    val time6 = new Time(2011, 9, 23, 16, 5, 0)
+    val solution3 = new Time(0,0,2,7,32,0)
+
+    val diff3 = time5.difference(time6)
+
+    assertEquals(solution3,diff3)
+  }
+
+  @Test def testLeapYear(): Unit = {
+    var isLeap = Time.IS_LEAP_YEAR(2016)
+    assertTrue(isLeap)
+
+    isLeap = Time.IS_LEAP_YEAR(2015)
+    assertFalse(isLeap)
+
+    isLeap = Time.IS_LEAP_YEAR(1900)
+    assertFalse(isLeap)
   }
 
 }
