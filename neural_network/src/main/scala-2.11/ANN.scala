fd4b455973bcd2672d4488e25dabdb20592eb5b4
feveraer <frederic.everaert@gmail.com>
2016-04-19 17:22:30 +0200

    nn: NeuralNetwork updated predictFromCsv, returns predicted data. ANN updated to draw wisp plot based on that data.

--- neural_network/src/main/scala-2.11/ANN.scala
+++ neural_network/src/main/scala-2.11/ANN.scala
@@ -1,4 +1,6 @@
 import ann.{NetworkTrainer, NeuralNetwork}
+import com.quantifind.charts.Highcharts._
+import com.quantifind.charts.highcharts.AxisType
 
 /**
   * Created by Frederic on 19/04/2016.
@@ -6,6 +8,7 @@ import ann.{NetworkTrainer, NeuralNetwork}
 object ANN {
   private val networkTrainer: NetworkTrainer = new NetworkTrainer("src/main/resources/TrainingsSet.tsv")
   private var ann: NeuralNetwork = null
+  private var data: Vector[(Seq[Long], Seq[Double])] = Vector.empty
 
   def main(args: Array[String]) {
     run
@@ -16,9 +19,21 @@ object ANN {
     networkTrainer.exportModel
     ann = new NeuralNetwork
     ann.loadModel("src/main/resources/network/encogNormalizationHelper.eg", "src/main/resources/network/encogBestMethod.eg")
-    ann.predictFromCSV("src/main/resources/TrainingsSet.tsv")
+    data = ann.predictFromCSV("src/main/resources/TrainingsSet.tsv")
+    draw
   }
 
   def draw {
+    // Expected data
+    line(data(0)._1.zip(data(0)._2))
+    hold()
+    // Predicted data
+    line(data(1)._1.zip(data(1)._2))
+    stack()
+    title("Neural network test")
+    xAxisType(AxisType.datetime)
+    xAxis("Time")
+    yAxis("Temperature in °C")
+    legend(List("Expected", "Predicted"))
   }
 }
--- neural_network/src/main/scala-2.11/ann/NeuralNetwork.scala
+++ neural_network/src/main/scala-2.11/ann/NeuralNetwork.scala
@@ -1,6 +1,7 @@
 package ann
 
 import java.io.{File, FileInputStream, ObjectInputStream}
+import java.util.Date
 
 import org.encog.ml.MLRegression
 import org.encog.ml.data.MLData
@@ -8,6 +9,7 @@ import org.encog.ml.data.versatile.NormalizationHelper
 import org.encog.persist.EncogDirectoryPersistence
 import org.encog.util.arrayutil.VectorWindow
 import org.encog.util.csv.ReadCSV
+import testsetann.{DateTime, DateTimeDifference}
 
 /**
   * Created by Lorenz on 19/04/2016.
@@ -32,7 +34,19 @@ class NeuralNetwork {
     bestMethod = EncogDirectoryPersistence.loadObject(new File(pathToBestModel)).asInstanceOf[MLRegression]
   }
 
-  def predictFromCSV(filename: String) {
+  def predictFromCSV(filename: String): Vector[(Seq[Long], Seq[Double])] = {
+    // Initialize empty Vector for output:
+    // Time series data - predicted or expected values
+    var outputVector: Vector[(Seq[Long], Seq[Double])] = Vector.empty
+    // time series sequence
+    var times: Seq[Long] = Seq.empty
+    // start date for time series
+    val startDate = new Date()
+    // predicted sequence
+    var predictedValues: Seq[Double] = Seq.empty
+    // expected sequence
+    var expectedValues: Seq[Double] = Seq.empty
+
     // Iterator for csv data:
     // filename - headers - format
     val csv: ReadCSV = new ReadCSV(filename, true, EncogConstants.FORMAT)
@@ -64,27 +78,35 @@ class NeuralNetwork {
 
       // Check if window is ready and there is enough data to build a full window.
       if (window.isReady) {
-        val result: StringBuilder = new StringBuilder
-
         // Copy the window.
         // data - start position
         window.copyWindow(input.getData, 0)
-        val correct: String = csv.get(numberOfColumns)
+
+        // DateTime in ms based on time difference
+        val datetime: Long = startDate.getTime + new DateTimeDifference(
+          csv.get(2).toInt, csv.get(3).toInt, csv.get(4).toInt, csv.get(5).toInt
+        ).toMillis()
+        // Expected output.
+        val correct: Double = csv.get(numberOfColumns).toDouble
 
         // Predict output.
         val output: MLData = bestMethod.compute(input)
 
         // Denormalize prediction.
-        val predicted: String = helper.denormalizeOutputVectorToString(output)(0)
-        result.append("Predicted:\t" + predicted + "\n")
-        result.append("Correct:\t" + correct + "\n")
-        result.append("\n")
-        System.out.println(result.toString)
+        val predicted: Double = helper.denormalizeOutputVectorToString(output)(0).toDouble
+
+        times = times :+ datetime
+        predictedValues = predictedValues :+ predicted
+        expectedValues = expectedValues :+ correct
       }
 
       // Add data to windows.
       window.add(slice)
       stopAfter -= 1
     }
+
+    outputVector = outputVector :+ (times, expectedValues)
+    outputVector = outputVector :+ (times, predictedValues)
+    outputVector
   }
 }
