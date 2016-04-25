9386338322ff82338e132e546bd18bfaae44671c
feveraer <frederic.everaert@gmail.com>
2016-04-25 14:31:40 +0200

    nn: updated CassandraWorker

--- neural_network/src/main/scala-2.11/workers/CassandraWorker.scala
+++ neural_network/src/main/scala-2.11/workers/CassandraWorker.scala
@@ -3,6 +3,8 @@ package workers
 import akka.actor.Actor
 import akka.util.ByteString
 import cassandra.CassandraConnection
+import json.TcpJsonLog
+import json.TcpJsonLog.Decoder
 
 /**
   * Created by Frederic on 25/04/2016.
@@ -10,8 +12,18 @@ import cassandra.CassandraConnection
 class CassandraWorker extends Actor {
 
   def receive = {
-    //case data: ByteString => CassandraConnection(new String(data.toByteBuffer.array()))
+    // Data received over TCP socket is of type ByteString
+    case data: ByteString => {
+
+      val jsonData = new String(data.toByteBuffer.array())
+      val log = Decoder.decodeLogJson(jsonData)
+
+      // Insert SensorLog in Cassandra DB
+      CassandraConnection.insertSensorLog(log.toSensorLog())
+      // Insert SensorInfo in Cassandra DB
+      CassandraConnection.insertSensorInfo(log.toSensorInfo())
+    }
     case default => println(default)
-  }
+}
 
 }
