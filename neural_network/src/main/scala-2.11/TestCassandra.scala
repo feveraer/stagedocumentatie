b828fcbe7bade84138a6061f2cf57a6742f232bf
Tyslan <verschingellorenz@hotmail.com>
2016-04-25 13:44:13 +0200

    nn: Cassandra test

--- neural_network/src/main/scala-2.11/TestCassandra.scala
+++ neural_network/src/main/scala-2.11/TestCassandra.scala
@@ -1,11 +1,10 @@
-package cassandra
-
+import cassandra.CassandraConnection
 import ssh_tunnel.SSHTunnel
 
 /**
   * Created by Lorenz on 25/04/2016.
   */
-object CassandraTest {
+object TestCassandra {
 
   def main(args: Array[String]) {
     SSHTunnel.connect("root", "Ugent2015")
