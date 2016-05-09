8e91ae68f88442b7660612bf9c3f8e0d50841988
Tyslan <verschingellorenz@hotmail.com>
2016-05-09 10:56:41 +0200

    data entry version 2 added

--- neural_network/src/main/scala-2.11/testsetann/DataEntry.scala
+++ neural_network/src/main/scala-2.11/testsetann/DataEntry.scala
@@ -1,6 +1,8 @@
 package testsetann
 
-import time.{DateTime, DateTimeDifference}
+import java.time.LocalDateTime
+
+import time.{DateTime, DateTimeDifference, TimeHelper}
 
 /**
   * Created by Lorenz on 13/04/2016.
@@ -8,5 +10,11 @@ import time.{DateTime, DateTimeDifference}
 case class DataEntry (time: DateTime, setTemp: Int, measuredTemp: Int) {
   def difference(entry:DataEntry): DateTimeDifference ={
     this.time.difference(entry.time)
+  }
+}
+
+case class DataEntryV2 (time: LocalDateTime, setTemp: Int, measuredTemp: Int) {
+  def difference(entry: DataEntryV2): Long ={
+    TimeHelper.differenceBetweenInMinutes(this.time, entry.time)
   }
 }
---
+++ neural_network/src/main/scala-2.11/testsetann/TransformerV2.scala
@@ -0,0 +1,8 @@
+package testsetann
+
+/**
+  * Created by Lorenz on 9/05/2016.
+  */
+object TransformerV2 {
+
+}
